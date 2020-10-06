85ee237ec6569ae381653f70b119e45c55bb6501
Sampson Kotey Djanie <sampson.kotey.djanie@gmail.com>
2020-10-06 06:07:39 +0000

    fourth commit

---
+++ src/List.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function List (){
+    return <ul>
+    <li>Ghana</li>
+    <li>Canada</li>
+    <li>Australia</li>
+    </ul>
+  }
+
+  export default List;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import Heading from './Heading';
 import Notes from './Notes';
 import Table from './Table';
+import List from './List';
 
 
 
@@ -12,6 +13,7 @@ ReactDOM.render(
     <div><Heading />
     <Notes />
     <Table />
+    <List />
     </div>,
 
   document.getElementById('root')
